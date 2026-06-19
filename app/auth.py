c2d365509acbcb170e166b7264594d7d8c08cfdd
PendragonXIII <pendragonxiiigarden@gmail.com>
2026-06-19 22:12:02 +0200

    Auth debug

--- app/auth.py
+++ app/auth.py
@@ -1,24 +1,9 @@
-import os
-
-from fastapi import Header
 from fastapi import HTTPException
 
 
-def verify_api_key(
+def verify_api_key():
 
-    x_api_key: str = Header(
-        default=None
+    raise HTTPException(
+        status_code=401,
+        detail="AUTH TEST"
     )
-
-):
-
-    expected_key = os.getenv(
-        "API_KEY"
-    )
-
-    if x_api_key != expected_key:
-
-        raise HTTPException(
-            status_code=401,
-            detail="Unauthorized"
-        )
